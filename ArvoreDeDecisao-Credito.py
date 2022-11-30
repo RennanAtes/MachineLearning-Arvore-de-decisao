c48a89afccf3a6372fb255ad1fcb02cda09c4b98
RennanAtes <108353021+RennanAtes@users.noreply.github.com>
2022-11-30 18:33:28 -0300

    Update ArvoreDeDecisao-Credito.py

--- ArvoreDeDecisao-Credito.py
+++ ArvoreDeDecisao-Credito.py
@@ -4,6 +4,7 @@ import pickle
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn import tree
+from sklearn.metrics import accuracy_score, classification_report
 basePrevendo= pd.read_csv("NovosClientes.csv") #Importando a base
 
 
@@ -20,7 +21,9 @@ with open('credit.pkl', 'rb') as f:
 arvore_credit = DecisionTreeClassifier(criterion='entropy', random_state = 0)
 arvore_credit.fit(X_credit_treinamento, y_credit_treinamento)
 PagaOuNao = arvore_credit.predict(NovosClientes)
+PagaOuNao = arvore_credit.predict(X_credit_teste)
 print (PagaOuNao)
+print (accuracy_score(y_credit_teste, previsoes)
 previsores = ['income', 'age', 'loan']
 fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (20,20))
 tree.plot_tree(arvore_credit, feature_names=previsores, class_names=['0','1'], filled=True)
